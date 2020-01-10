c4ef62feb58b48b3af363d71ad8e1ebfb463a39f
Lovre Budimir <lovre-antonio.budimir@fer.hr>
2020-01-10 05:34:46 +0100

    rename of experiment_method_v1, code cleanup, added match function and testing function

--- metod_A.cpp
+++ metod_A.cpp
@@ -29,6 +29,7 @@ std::vector<std::string> readFastQFile(std::string file)
             switch (i % 4)
             {
             case 1:
+                line.erase(remove_if(line.begin(), line.end(), isspace), line.end());  //remove whitespace              
                 sequences.push_back(line);
                 break;
 
@@ -39,6 +40,33 @@ std::vector<std::string> readFastQFile(std::string file)
         }
     }
     return sequences;
+}
+
+std::vector<std::string> readFastaFile(std::string file)
+{
+    std::ifstream fastq(file);
+    std::vector<std::string> alels;
+    std::string line;
+
+    int i = 0;
+    if (fastq.is_open())
+    {
+        while (getline(fastq, line))
+        {
+            switch (i % 2)
+            {
+            case 1:
+                line.erase(remove_if(line.begin(), line.end(), isspace), line.end());  //remove whitespace        
+                alels.push_back(line);
+                break;
+
+            default:
+                break;
+            }
+            i++;
+        }
+    }
+    return alels;
 }
 
 /**
@@ -98,12 +126,37 @@ std::vector<std::string> find_sequences_with_most_common_length_plus_minus_n(std
  * @param two strings
  * @return distance 
  */
-int calculate_distance(std::string s1, std::string s2){
+int hamming_distance(std::string s1, std::string s2){
 
     int i = 0, count = 0; 
     
     for(i = 0; i < s1.size(); ++i){
         if(s1[i] != s2[i]){
+            count += 1;
+        }
+    }
+    return count; 
+
+}
+
+/**
+ * @author Lovre Budimir
+ * Calculate same distance between two strings
+ * 
+ * @param two strings
+ * @return distance 
+ */
+int match_distance(std::string s1, std::string s2){
+
+    if(s1.length() != s2.length()){
+         fprintf(stderr, "DIFFRENT STRING SIZES");
+        return -1;
+    }
+
+    int i = 0, count = 0; 
+    
+    for(i = 0; i < s1.size(); ++i){
+        if(s1[i] == s2[i]){
             count += 1;
         }
     }
@@ -158,7 +211,7 @@ std::map<int, std::string> init_clusters(std::vector<std::string> msa, int k){
 
             std::string centroid = (it2->second);
             
-            int distance = calculate_distance(current, centroid);
+            int distance = hamming_distance(current, centroid);
 
             if(distance < k) {
                 flag = 0;
@@ -197,7 +250,7 @@ std::map<int, std::vector<std::string>> create_clusters(std::vector<std::string>
 
             std::string current = (*it);
 
-            if(calculate_distance(current, centroid) < k){
+            if(hamming_distance(current, centroid) < k){
                 clusters[cluster_id].push_back(current);
             }
         }
@@ -228,14 +281,119 @@ std::map<int, std::vector<std::string>> clean_clusters(std::map<int, std::vector
     return clusters;
 }
 
+std::vector<std::string> find_allels(std::map<int, std::vector<std::string>> clusters,  int alg, int m, int n, int g, int e){
+
+    std::vector<std::string> consensuses;
+
+    for(std::map<int, std::vector<std::string>>::iterator it = clusters.begin(); it != clusters.end(); it++){
+
+        std::vector<std::string> cluster_sequences = it->second;
+
+        // parametri: AlignmentType (0 - lokalno, 1 - globalno, 2 - poluglobalno), m (podudaranje), n (zamjena), g (umetanje), e (brisanje)
+        auto alignment_engine = spoa::createAlignmentEngine(static_cast<spoa::AlignmentType>(alg), m, n, g, e);
+
+        auto graph = spoa::createGraph();
+
+        for (const auto &it : cluster_sequences)
+        {
+            auto alignment = alignment_engine->align(it, graph);
+            graph->add_alignment(alignment, it);
+        }
+
+        std::string consensus = graph->generate_consensus();
+
+
+        // fprintf(stderr, "Alel (%zu)\n", consensus.size());
+        // fprintf(stderr, "%s\n\n", consensus.c_str());
+
+        consensuses.push_back(consensus);
+
+    }
+
+    return consensuses;
+
+
+}
+
+/**
+ * @author Lovre Budimir
+ * find corresponding consensus for every expected allele if possible
+ * 
+ * @param consensuses
+ */
+void test_results(std::vector<std::string> expected, std::vector<std::string> predicted){
+
+    printf("TESTING: find corresponding consensus for every expected allele if possible");
+
+    int expected_id = 1;
+
+    for (const auto &true_alel : expected){
+
+        int predicted_id = 1;
+        
+        int true_length = true_alel.length();
+
+        std::string best_predicted_msa;
+        std::string best_expected_msa;
+        int best_predicted_id = -1;
+        double best_acc = 0;
+        int best_matches = -1;
+
+
+        for (const auto &pred_alel : predicted){
+
+            // globalno poravnanje za testiranje
+            auto alignment_engine = spoa::createAlignmentEngine(static_cast<spoa::AlignmentType>(2), 4, -1, -2, -2);
+
+            auto graph = spoa::createGraph();
+
+            auto alignment1 = alignment_engine->align(true_alel, graph);
+            graph->add_alignment(alignment1, true_alel);
+
+            auto alignment2 = alignment_engine->align(pred_alel, graph);
+            graph->add_alignment(alignment2, pred_alel);
+
+            std::vector<std::string> msa;
+            graph->generate_multiple_sequence_alignment(msa);
+
+            int matches = match_distance(msa[0], msa[1]);
+            double acc = (double)matches / (double) true_length;
+
+            if(acc > best_acc){
+                best_predicted_id = predicted_id;
+                best_expected_msa = msa[0];
+                best_predicted_msa = msa[1];
+                best_acc = acc;
+                best_matches = matches;
+            }
+
+            predicted_id++;
+        }
+
+        // nesto nije u redu, duljina expecteda i predicteda je ista, ali u consoli se ne ispise visak na kraju do kraja
+        fprintf(stderr, "Expected(%d) and Predicted(%d)\n", expected_id, best_predicted_id);fflush(stdout);
+        fprintf(stderr, "%s\n\n", best_expected_msa.c_str()); fflush(stderr);
+        fprintf(stderr, "%s\n\n", best_predicted_msa.c_str()); fflush(stderr);
+
+        double perc = best_acc * 100;
+        fprintf(stderr, "Acc: %.2f%% (%d/%d)\n\n\n", perc, best_matches, true_length); fflush(stderr);        
+
+        expected_id++;
+    
+    }
+
+
+}
+
+
+
 int main(int argc, char **argv){
 
     std::vector<std::string> allSequences = readFastQFile("./fastq/J30_B_CE_IonXpress_006.fastq");
 
     std::vector<std::string> sequences = find_sequences_with_most_common_length_plus_minus_n(allSequences, 5);
 
-    auto alignment_engine = spoa::createAlignmentEngine(static_cast<spoa::AlignmentType>(atoi(argv[1])),
-                                                        atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    auto alignment_engine = spoa::createAlignmentEngine(static_cast<spoa::AlignmentType>(atoi(argv[1])), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
 
     auto graph = spoa::createGraph();
 
@@ -258,28 +416,24 @@ int main(int argc, char **argv){
 
     std::map<int, std::vector<std::string>> final_clusters = clean_clusters(filtered_clusters);
 
+    std::vector<std::string> consensuses = find_allels(final_clusters, atoi(argv[1]),atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
     
-    
-    for(std::map<int, std::vector<std::string>>::iterator it = final_clusters.begin(); it != final_clusters.end(); it++){
 
-        std::vector<std::string> cluster_sequences = it->second;
+    int test = 1;
 
-        auto alignment_engine2 = spoa::createAlignmentEngine(static_cast<spoa::AlignmentType>(atoi(argv[1])),
-                                                        atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    if(test){
 
-        auto graph2 = spoa::createGraph();
+        std::vector<std::string> expected = readFastaFile("./J30B_expected.fasta");
 
-        for (const auto &it : cluster_sequences)
-        {
-            auto alignment2 = alignment_engine2->align(it, graph2);
-            graph2->add_alignment(alignment2, it);
+        test_results(expected, consensuses);
+
+    }else{
+
+        for (const auto &it : consensuses){
+            fprintf(stderr, "Alel (%zu)\n", it.size());
+            fprintf(stderr, "%s\n\n", it.c_str());
         }
 
-        std::string consensus2 = graph2->generate_consensus();
-        fprintf(stderr, "Alel (%zu)\n", consensus2.size());
-        fprintf(stderr, "%s\n\n", consensus2.c_str());
-
     }
-
 
 }
