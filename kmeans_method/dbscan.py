51032631473c06c5a974ba6a5fa7785301414bb9
Luka Justić <jluka24@gmail.com>
2020-01-11 10:29:18 +0100

    Moved dbscan.py to new folder

--- kmeans_method/dbscan.py
+++ kmeans_method/dbscan.py
@@ -19,11 +19,28 @@ data = load_data('J30_B_CE_IonXpress_006.fastq')
 data = data[0:1000]
 print(len(data))
 
-
 def lev_metric(x, y):
      i, j = int(x[0]), int(y[0])     # extract indices
      return levenshtein(data[i], data[j])
 
 X = np.arange(len(data)).reshape(-1, 1)
 
-print(dbscan(X, metric=lev_metric, eps=5, min_samples=50, algorithm='brute'))
+items, labels = dbscan(X, metric=lev_metric, eps=5, min_samples=50, algorithm='brute')
+
+#print("Items: ", items)
+print("Labels: ", labels)
+
+clusters = {}
+
+for index, label in enumerate(labels):
+    if label == -1:
+        continue
+    
+    if label not in clusters:
+        clusters[label] = []
+
+    print(index)
+    clusters[label].append(data[index])
+
+print(clusters)
+
