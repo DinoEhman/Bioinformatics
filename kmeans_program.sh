0b89a7b310cbe8303349724bbb60ab05d2a4ef58
Dino Ehman <dinoehman@hotmail.com>
2020-01-14 18:30:31 +0100

    updated results and some compilation programs

--- kmeans_program.sh
+++ kmeans_program.sh
@@ -1,6 +1,3 @@
-./kmeans_method/k_means_preprocess 0 5 -4 -8 -6 files/fastq/J30_B_CE_IonXpress_006.fastq kmeans_method/kmeans_input_30.txt
-
+./kmeans_method/k_means_preprocess 1 0 -1 -1 -1 files/fastq/J30_B_CE_IonXpress_006.fastq kmeans_method/kmeans_input_30.txt
 python kmeans_method/kmeans.py kmeans_method/kmeans_input_30.txt kmeans_method/kmeans_output_30.txt
-
-
-./kmeans_method/k_means_postprocess 0 5 -4 -8 -6 kmeans_method/kmeans_output_30.txt kmeans_method/kmeans_results_30.txt
+./kmeans_method/k_means_postprocess 1 0 -1 -1 -1 kmeans_method/kmeans_output_30.txt kmeans_method/kmeans_results_30.txt
